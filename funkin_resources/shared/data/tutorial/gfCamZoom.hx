dff9198b771b0bdfd39caef0f2514358cc78e54e
Homura <egzozu.be.bas@gmail.com>
2026-02-18 00:58:53 +0300

    broken but Tutorial Erect

--- funkin_resources/shared/data/tutorial/gfCamZoom.hx
+++ funkin_resources/shared/data/tutorial/gfCamZoom.hx
@@ -1,12 +1,26 @@
+import backend.Difficulty;
+
 var cameraTween:FlxTween;
+
+function onCreate()
+{
+	if (Difficulty.getString().toLowerCase() == "erect" || Difficulty.getString().toLowerCase() == "nightmare")
+		game.opponentCameraOffset = [-50, 0];
+}
 
 function opponentNoteHit(note:Note)
 {
+	if (Difficulty.getString().toLowerCase() == "erect" || Difficulty.getString().toLowerCase() == "nightmare")
+		return;
+
 	game.camZooming = false;
 }
 
 function onMoveCamera(focus:String)
 {
+	if (Difficulty.getString().toLowerCase() == "erect" || Difficulty.getString().toLowerCase() == "nightmare")
+		return;
+
 	if (focus == 'boyfriend')
 	{
 		if (cameraTween == null && FlxG.camera.zoom != 1)
